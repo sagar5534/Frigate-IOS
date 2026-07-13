f1e27a43eb500c8bb9a71ae55b176b7078c82df5
Sagar Patel <s.72427patel@gmail.com>
2026-07-13 13:05:18 -0400

    Base Initialization

---
+++ app/Frigate/FrigateApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrigateApp.swift
+//  Frigate
+//
+//  Created by Sagar Patel on 2026-07-13.
+//
+
+import SwiftUI
+
+@main
+struct FrigateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
